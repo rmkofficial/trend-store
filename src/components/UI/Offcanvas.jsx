fc11b260215a3fb2e7565debf07479ab323871fc
Ramazan Mert Kaya <kmert0@outlook.com>
2024-03-05 00:07:06 +0300

    Add Offcanvas component and update index.html

--- src/components/UI/Offcanvas.jsx
+++ src/components/UI/Offcanvas.jsx
@@ -1,13 +1,26 @@
 import "./Offcanvas.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = ({ onClose }) => {
+  return <div className="backdrop" onClick={onClose}></div>;
+};
+const OffcanvasOverlay = ({ children }) => {
+  return (
+    <div className="offcanvas">
+      <div className="content">{children}</div>
+    </div>
+  );
+};
 
 const Offcanvas = ({ children, onClose }) => {
+  const portalElement = document.getElementById("overlays");
   return (
     <>
-      <div className="backdrop" onClick={onClose}>
-        <div className="offcanvas">
-          <div className="content">{children}</div>
-        </div>
-      </div>
+      {ReactDOM.createPortal(<Backdrop onClose={onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <OffcanvasOverlay>{children}</OffcanvasOverlay>,
+        portalElement
+      )}
     </>
   );
 };
